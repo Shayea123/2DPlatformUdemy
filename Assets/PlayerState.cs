f9c808e53fcadd4c278a8dd8ec1f6e752a0eaea1
Shayea <67074046+Shayea123@users.noreply.github.com>
2024-08-16 18:11:25 +0300

    second nothing much

--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
     {
-        
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Enter()
     {
-        
+
     }
+
+    public virtual void Update() 
+    {
+    
+    }
+
+    public virtual void Exit() 
+    {
+    
+    }
+
 }
